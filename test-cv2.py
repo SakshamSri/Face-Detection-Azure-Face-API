caee01f3fba93b1614b608f15ac097b31d59ecff
Saksham Srivastava <saksham19.1996@gmail.com>
2019-10-15 17:58:34 +0530

    test-cv-second

--- test-cv2.py
+++ test-cv2.py
@@ -1,30 +1,36 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+def run_video_capture():
+    
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-cap = cv2.VideoCapture(0)
-
-while 1:
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    cap = cv2.VideoCapture(0)
     flag = 0
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img, (x,y), (x+w,y+h), (255,255,0), 2)        
-        # eyes = eye_cascade.detectMultiScale(roi_gray)
-        detected_face = img[y:y+h, x:x+w]
-        status = cv2.imwrite('face_detected.jpg', detected_face)
-        flag = 1
-        break
-        # for(ex, ey, ew, eh) in eyes:
-    if flag == 1:
-        break
-    cv2.imshow('img', img)
-    # status = cv2.imwrite('face_detected.jpg', img)
+    count = 0
+    while 1:
+        ret, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img, (x,y), (x+w+10,y+h+10), (255,255,0), 2)        
+            detected_face = img[y:y+h+10, x:x+w+10]
+            if flag == 4 :
+                status = cv2.imwrite('face_detected' + str(count) + '.jpg', detected_face)
+                count = count+1
+        flag = flag+1
+        print(flag)
+        if flag > 4:
+            flag = 0
+            # break
+        cv2.imshow('img', img)
+        # status = cv2.imwrite('face_detected.jpg', img)
 
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
 
-cap.release()
+    # cap.release()
+    # cv2.destroyWindow('img')
 # cv2.destroyAllWindows()
+run_video_capture()
